d500d22649ff7f20f595ee11c91bc7e77ac3ecbd
Radoslav22 <4takvr26@solent.ac.uk>
2020-10-27 10:44:20 +0000

    Added code to demonstrate a simple set.

---
+++ data/sets/simple_set.py
@@ -0,0 +1,11 @@
+#create a program to help Beep and Bop record what they can see in the distance.
+#function
+def observed ():
+    observations = {"Flying Car", "Sky Scraper", "Laser", "Dome"}
+    return observations 
+
+def run ():
+    record  = observed () 
+    print (record)
+
+run()
