77420b913951326357383e775ea78a9a20c3b9c8
Radoslav22 <4takvr26@solent.ac.uk>
2020-10-27 14:46:16 +0000

    Added code to demonstrate a simple dictionary.

---
+++ data/dicts/simple_dict.py
@@ -0,0 +1,13 @@
+#We wish to create a program to help Beep and Bop decipher the pattern
+#first function with no parameters
+def pattern ():
+    sequences = {"Short Sequence":3, "Medium Sequence":2, "Long Sequence":1}
+    #return the dictionary
+    return sequences
+#second function with no parameters 
+def run ():
+    way = pattern()
+    print(way)
+
+#call the second function
+run()
