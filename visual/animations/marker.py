e6ad11472538a9bca8576626c23d3f91df52b9ae
Radoslav22 <4takvr26@solent.ac.uk>
2020-11-24 12:24:28 +0000

    Added code to visualise a sine wave.

--- visual/animations/marker.py
+++ visual/animations/marker.py
@@ -1,2 +1,23 @@
-# -*- coding: utf-8 -*-
+import matplotlib.pyplot as plt   
+import matplotlib.animation as animation
+
+
+fig, ax = plt.subplots()
+
+
+
+
+ax.cla()
+ax.set_xlim(0, 10)
+ax.set_ylim(0, 10) 
+ax.plot(frame,frame,'ro')
+
+
+
+simple_animation=animation.FuncAnimation(fig, animate, frames=10, interval=100)
+
+plt.show()
+
+
+
 
